1b346cd5adb2dcb271bce5942dd5a36175380bf6
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-25 13:48:32 +0200

    Zadanie 4 - Parallel

---
+++ src/main/resources/db/migration/V1.0.4__create_notification.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `notification` (
+
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `tresc` varchar(255)
+);
+
+
+INSERT INTO notification (id,tresc)
+VALUES ('1', 'powiadomienie 1');
+
+INSERT INTO notification (id,tresc)
+VALUES ('2', 'powiadomienie 2');
+
+INSERT INTO notification (id,tresc)
+VALUES ('3', 'powiadomienie 3');
+
+INSERT INTO notification (id,tresc)
+VALUES ('4', 'powiadomienie 4');

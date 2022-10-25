0a262a9d3366ece148f76fe88326935353c476f9
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-25 16:04:19 +0200

    gr2 DMN

--- src/main/java/com/example/workflow/mvc/processes/RegistryAdditionalHoursProcess.java
+++ src/main/java/com/example/workflow/mvc/processes/RegistryAdditionalHoursProcess.java
@@ -4,5 +4,7 @@ public class RegistryAdditionalHoursProcess {
 
     public static String APPLICATION_ID = "applicationId";
     public static String REGISTRY_LIST = "registryList";
+    public static String ADDITIONAL_HOURS = "additionalHours";
+    public static String DAY_OF_MONTH = "dayOfMonth";
 
 }

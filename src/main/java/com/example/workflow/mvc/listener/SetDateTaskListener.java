83ea5e5828c011ddadff8878e833be372f5379a0
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-26 15:37:26 +0200

    Zadanie 7 - Timer

--- src/main/java/com/example/workflow/mvc/listener/SetDateTaskListener.java
+++ src/main/java/com/example/workflow/mvc/listener/SetDateTaskListener.java
@@ -5,13 +5,13 @@ import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 @Component
 public class SetDateTaskListener implements ExecutionListener {
 
     @Override
     public void notify(DelegateExecution delegateExecution) throws Exception {
-        delegateExecution.setVariable("someDate", LocalDate.now());
-//        delegateExecution.getProcessEngine().getTaskService().createTaskQuery().taskId()
+        delegateExecution.setVariable("someDate", new Date());
     }
 }

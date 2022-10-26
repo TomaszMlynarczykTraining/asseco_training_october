b4e610bc542c32d579d90e4e8ee8e7da8377ac5d
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-26 15:09:28 +0200

    Poprawa zadania

--- src/main/java/com/example/workflow/mvc/listener/G1StartInvoiceVerify.java
+++ src/main/java/com/example/workflow/mvc/listener/G1StartInvoiceVerify.java
@@ -1,7 +1,9 @@
 package com.example.workflow.mvc.listener;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.camunda.bpm.engine.delegate.TaskListener;
 import org.camunda.bpm.engine.task.Task;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +11,11 @@ import java.util.Date;
 import java.util.List;
 
 @Component
-public class G1StartInvoiceVerify implements ExecutionListener {
+public class G1StartInvoiceVerify implements TaskListener {
+
 
     @Override
-    public void notify(DelegateExecution delegateExecution) throws Exception {
-        List<Task> tasks = delegateExecution.getProcessEngine().getTaskService().createTaskQuery().active().processInstanceId(delegateExecution.getCurrentActivityId()).list();
-        if (tasks != null && !tasks.isEmpty()) {
-            tasks.get(0).setDueDate(new Date());
-        }
+    public void notify(DelegateTask delegateTask) {
+        delegateTask.setDueDate(new Date());
     }
 }

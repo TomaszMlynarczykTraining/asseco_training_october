4f23b7b279b072256c72cbaf109337d3828e4d10
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-27 13:30:03 +0200

    messages

--- src/main/java/com/example/workflow/mvc/delegates/CorellateMessage.java
+++ src/main/java/com/example/workflow/mvc/delegates/CorellateMessage.java
@@ -5,6 +5,7 @@ import com.example.workflow.mvc.service.ClientService;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,13 @@ public class CorellateMessage implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
-        runtimeService.correlateMessage("MSG_START_PR02");
+        MessageCorrelationResult someMessage = delegateExecution.getProcessEngineServices()
+                .getRuntimeService()
+                .createMessageCorrelation("otherMessageName").correlateWithResult();
+        //podobnie jak powyzej, mozemy uzyc tego kodu w sekcji Implementation -> Expression
+        // .correlate() - nie zwraca wyniku
+               // .correlateWithResult(); - zwraca nam wynik, wyrzuca incydent
+        System.out.println(someMessage);
+       // runtimeService.correlateMessage("MSG_START_PR02");
     }
 }

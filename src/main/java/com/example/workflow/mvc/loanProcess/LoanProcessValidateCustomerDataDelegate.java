3db1a4745bc0bc052dcc1859d8bd0b110057aab2
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-28 15:01:19 +0200

    Grupa1LoanProcess-2

---
+++ src/main/java/com/example/workflow/mvc/loanProcess/LoanProcessValidateCustomerDataDelegate.java
@@ -0,0 +1,53 @@
+package com.example.workflow.mvc.loanProcess;
+
+
+import com.example.workflow.mvc.entity.Client;
+import com.example.workflow.mvc.repository.ClientRepository;
+import com.example.workflow.mvc.service.ClientService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LoanProcessValidateCustomerDataDelegate implements JavaDelegate {
+
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    ClientService clientService;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        Integer id = (Integer) delegateExecution.getVariable(LoanProcessVariable.ID);
+        String street = (String) delegateExecution.getVariable(LoanProcessVariable.STREET);
+        String phoneNumber = (String) delegateExecution.getVariable(LoanProcessVariable.PHONE_NUMBER);
+        String declaredIncome = (String) delegateExecution.getVariable(LoanProcessVariable.DECLARED_INCOME);
+        String currency = (String) delegateExecution.getVariable(LoanProcessVariable.CURRENCY);
+
+        delegateExecution.setVariable(LoanProcessVariable.IS_CORRECT, true);
+
+        Optional<Client> optionalClient = clientRepository.findById(id.longValue());
+
+        if (optionalClient.isPresent()) {
+            if (!optionalClient.get().getStreet().equals(street) ||
+                    !optionalClient.get().getPhoneNumber().equals(phoneNumber) ||
+                    !optionalClient.get().getDeclaredIncome().equals(declaredIncome) ||
+                    !optionalClient.get().getCurrency().equals(currency)
+            ) {
+                delegateExecution.setVariable(LoanProcessVariable.IS_CORRECT, false);
+            }
+        } else {
+            delegateExecution.setVariable(LoanProcessVariable.IS_CORRECT, false);
+        }
+
+    }
+}
--- src/main/java/com/example/workflow/mvc/loanProcess/LoanProcessVariable.java
+++ src/main/java/com/example/workflow/mvc/loanProcess/LoanProcessVariable.java
@@ -5,4 +5,14 @@ public class LoanProcessVariable {
     public static String IS_CORRECT = "isCorrect";
     public static String LOAN_PROCESS_TYPE = "loanProcessType";
 
+    public static String ID = "customerID";
+
+    public static String STREET = "street";
+
+    public static String PHONE_NUMBER = "phoneNumber";
+
+    public static String DECLARED_INCOME = "declaredIncome";
+
+    public static String CURRENCY = "currency";
+
 }

fbd126db1b7b3911065a7b37a012aee9d9b3a430
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-28 14:44:55 +0200

    Zadanie 10 - Nowy projekt service task added

--- src/main/java/com/example/workflow/mvc/houseLoan/HouseLoanService.java
+++ src/main/java/com/example/workflow/mvc/houseLoan/HouseLoanService.java
@@ -1,6 +1,9 @@
 package com.example.workflow.mvc.houseLoan;
 
+import com.example.workflow.mvc.entity.Client;
+import com.example.workflow.mvc.entity.Debt;
 import com.example.workflow.mvc.entity.Notification;
+import com.example.workflow.mvc.repository.ClientRepository;
 import com.example.workflow.mvc.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -9,21 +12,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
-public class HouseLoanService implements JavaDelegate {
+public class HouseLoanService implements JavaDelegate{
 
     @Autowired
-    private NotificationRepository notificationRepository;
+    private ClientRepository clientRepository;
 
-    public List<Notification> getNotification() {
-        return notificationRepository.findAll();
+    public Debt getClientsDebt(Long id) {
+        Optional<Client> optionalClient = clientRepository.findById(id);
+        return optionalClient.map(Client::getDebt).orElse(null);
     }
+
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        System.out.println("psmgpkdfs");
+        Long clientId = Long.parseLong((String) delegateExecution.getVariable("clientId"));
+        Debt debt = getClientsDebt(clientId);
+        delegateExecution.setVariable("debt", Long.parseLong(debt.getAmount()));
     }
 }

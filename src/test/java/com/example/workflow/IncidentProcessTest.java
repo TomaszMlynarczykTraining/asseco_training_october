b922f3315c326572bc4f297f45fcc21eb400a86b
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-10-31 16:03:05 +0100

    unit tests

---
+++ src/test/java/com/example/workflow/IncidentProcessTest.java
@@ -0,0 +1,38 @@
+package com.example.workflow;
+
+import com.example.workflow.mvc.delegates.Somebean;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
+import org.camunda.community.mockito.DelegateExpressions;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
+
+@Deployment(resources = {
+        "processes/incident_test.bpmn"
+})
+public class IncidentProcessTest  extends  AbstractProcessEngineRuleTest {
+
+
+    @Mock
+    Somebean somebean;
+
+    @Test
+    public void testHappyPath(){
+        DelegateExpressions.registerJavaDelegateMock("someBean");
+// umiescic implementacje dla procesu podrzednego
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("Process_10n1nyt");
+        assertThat(processInstance).isWaitingAt("Activity_0eyz6ov");
+
+        Task task = taskService().createTaskQuery().processInstanceId(processInstance.getProcessInstanceId()).singleResult();
+        complete(task);
+
+        assertThat(processInstance).isWaitingAt("Event_0qdik5i");
+        execute(job("Event_0qdik5i"));
+
+        assertThat(processInstance).isEnded();
+    }
+}

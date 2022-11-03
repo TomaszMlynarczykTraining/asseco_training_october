d4a63c47e15b3c8ed14cbadcb5e00d0650a95c5c
Tomasz Mlynarczyk <tomasz.feliks.mlynarczyk@gmail.com>
2022-11-03 12:17:07 +0100

    external task get currency - simple get url grupa 3

--- src/main/java/com/example/workflow/mvc/external_task/CurrencyExternalTask.java
+++ src/main/java/com/example/workflow/mvc/external_task/CurrencyExternalTask.java
@@ -7,11 +7,14 @@ import org.camunda.bpm.client.task.ExternalTaskService;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 
 @Component
 @ExternalTaskSubscription("getCurrency") // create a subscription for this topic name
 public class CurrencyExternalTask implements ExternalTaskHandler {
+
+    public static final String SERVER_URI = "https://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json";
     @Override
     public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
         System.out.println(externalTask.getBusinessKey());
@@ -20,6 +23,11 @@ public class CurrencyExternalTask implements ExternalTaskHandler {
 
         variables.put("currency", "Batman");
 
+        RestTemplate restTemplate = new RestTemplate();
+        Response currencyResp = restTemplate.getForObject(SERVER_URI, Response.class);
+        System.out.println(currencyResp);
+
+        variables.put("currencyFromNbp", currencyResp.getCurrency());
 
         externalTaskService.complete(externalTask, variables);
     }
